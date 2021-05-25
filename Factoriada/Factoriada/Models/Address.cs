30f36a038f2860968327edf4b2df3f16dc16f961
Dode <dodebogdan@yahoo.com>
2021-05-25 19:38:27 +0300

    Program initialize

---
+++ Factoriada/Factoriada/Models/Address.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Factoriada.Models
+{
+    class Address
+    {
+        public Guid AddressId { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+    }
+}
---
+++ Factoriada/Factoriada/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Factoriada.Models
+{
+    class User
+    {
+        public Guid UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Address AddressPlace { get; set; }
+    }
+}
