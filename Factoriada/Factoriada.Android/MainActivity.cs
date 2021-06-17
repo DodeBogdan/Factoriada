6d960fcb810fbe6087631e29f9cdb56c9c23b0ae
Bogdan Gheorghe <dodebogdan@yahoo.com>
2021-06-17 16:06:29 +0300

    Added new logo.

--- Factoriada/Factoriada.Android/MainActivity.cs
+++ Factoriada/Factoriada.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using Acr.UserDialogs;
 
 namespace Factoriada.Droid
 {
-    [Activity(Label = "Factoriada", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Factoriada", Icon = "@mipmap/factoriadaLogo", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
